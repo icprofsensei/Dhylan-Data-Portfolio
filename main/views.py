daa153d0e134571ab019717d7dce15ba7f880882
Dhylan Patel <dhylan@live.co.uk>
2024-11-16 20:22:05 +0000

    Initial Commit msg

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from datetime import date
+
+
+def home(request):
+    return render(request, 'home.html')  # Renders the home.html template
