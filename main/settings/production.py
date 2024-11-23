3e0ed4f46e91e377cc9872c633fd10e2d79155f9
Dhylan Patel <dhylan@live.co.uk>
2024-11-23 11:56:55 +0000

    staticworking1

--- main/settings/production.py
+++ main/settings/production.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('RENDER_EXTERNAL_HOSTNAME')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
-    BASE_DIR / "main/static",  # Update this path as needed
+    BASE_DIR / "static",  # Update this path as needed
 ]
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # Handle CORS headers
